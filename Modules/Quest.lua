50b6001a1a59d6ce8f58820809f3042eeb3e962c
Phanx <addons@phanx.net>
2011-12-11 11:00:34 +0000

    - TOC 40300

--- Modules/Quest.lua
+++ Modules/Quest.lua
@@ -2,7 +2,6 @@
 	Hydra
 	Multibox leveling helper.
 	Written by Phanx <addons@phanx.net>
-	Maintained by Akkorian <akkorian@hotmail.com>
 	Copyright © 2010–2011 Phanx. Some rights reserved. See LICENSE.txt for details.
 	http://www.wowinterface.com/downloads/info17572-Hydra.html
 	http://wow.curse.com/downloads/wow-addons/details/hydra.aspx
